5421e09c7cf35510dada75d9e0a6093b93333258
KunalKaushal-boop <75515019+KunalKaushal-boop@users.noreply.github.com>
2025-04-14 18:51:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import pickle
 import requests
 import gdown
 import os
-import pandas as pd
+
 @st.cache_resource
 def fetch_poster(movie_title):
-    url=f"http://www.omdbapi.com/?t={movie_title}&apikey=d074f616"
+    url = f"http://www.omdbapi.com/?t={movie_title}&apikey=d074f616"
     response = requests.get(url)
     data = response.json()
     return data.get('Poster', 'https://via.placeholder.com/300x450?text=No+Poster+Found')
-#load the data
-movies=pickle.load(open('movies.pkl', 'rb'))
-#loading similarity file
-# similarity=pickle.load(open('similarity.pkl', 'rb'))
+
+# Load movie data
+movies = pickle.load(open('movies.pkl', 'rb'))
+
+# Load similarity matrix
 def load_similarity():
     output = 'similarity.pkl'
     if not os.path.exists(output):
@@ -24,38 +25,35 @@ def load_similarity():
 
 similarity = load_similarity()
 
-
-movie_list=movies['title'].values
-
-st.title('Movie Recommender System')
-
+# recommend function
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
-    # if we sort, we will loose all the index positions
     distances = similarity[movie_index]
     movies_indices = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    recommend_movies=[]
-    recommend_posters=[]
+    recommended_movies = []
+    recommended_posters = []
     for i in movies_indices:
-        movie_title=movies.iloc[i[0]].title
-        #fetch poster from API
-        recommend_posters.append(fetch_poster(movie_title))
-        recommend_movies.append(movie_title)
-        # print(movies_list.iloc[i[0]].title)
-        # print(i[0])
-    return recommend_movies, recommend_posters
+        movie_title = movies.iloc[i[0]].title
+        recommended_movies.append(movie_title)
+        recommended_posters.append(fetch_poster(movie_title))
+    return recommended_movies, recommended_posters
 
-#streamlit select box
-selected_movie_name = st.selectbox(
-    "Select a movie get recommendations:",
-    movie_list
-)
+st.title("🎬 Movie Recommender System")
 
-#on button click
-if st.button("Recommend"):
-    names,posters=recommend(selected_movie_name)
-    cols=st.columns(5)
+# Movie selector
+selected_movie = st.selectbox("Select a movie to get recommendations:", movies['title'].values)
+
+# Recommend button and output
+if st.button("🎥 Recommend"):
+    with st.spinner('Fetching recommendations...'):
+        names, posters = recommend(selected_movie)
+
+    # Display recommendations with columns
+    st.subheader("🎯 Top 5 Recommendations:")
+    cols = st.columns(5)
     for i in range(5):
         with cols[i]:
-            st.text(names[i])
-            st.image(posters[i])
+            st.image(posters[i], use_container_width=True)
+            st.markdown(f"**{names[i]}**", unsafe_allow_html=True)
+
+    st.balloons() 
